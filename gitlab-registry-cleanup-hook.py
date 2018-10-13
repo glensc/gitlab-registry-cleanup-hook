4016065f17541ecaf1b260b171bb942c030c4a0f
Elan Ruusamäe <glen@delfi.ee>
2018-10-13 11:15:16 +0300

    indent cosmetics

--- gitlab-registry-cleanup-hook.py
+++ gitlab-registry-cleanup-hook.py
@@ -37,35 +37,37 @@ def validate():
 def cleanup():
     data = request.json
     state = data['object_attributes']['state']
-    if state == 'merged':
-        logger.info("Merge detected")
-        branch = data['object_attributes']['source_branch']
-        path_with_namespace = data['object_attributes']['source']['path_with_namespace'].split("/")
-        project_namespace =  path_with_namespace[0]
-        project_name = path_with_namespace[1]
-        registry_data_dir = "/var/opt/gitlab/gitlab-rails/shared/registry/docker/registry/v2"
-        image = "%s/%s" % ( project_namespace, project_name )
-        tag = branch
-        dry_run = False
-        untagged = False
-        prune = True
+    if state != 'merged':
+        return
 
-        logger.info("Trying to delete %s:%s" %( image, branch ))
+    logger.info("Merge detected")
+    branch = data['object_attributes']['source_branch']
+    path_with_namespace = data['object_attributes']['source']['path_with_namespace'].split("/")
+    project_namespace =  path_with_namespace[0]
+    project_name = path_with_namespace[1]
+    registry_data_dir = "/var/opt/gitlab/gitlab-rails/shared/registry/docker/registry/v2"
+    image = "%s/%s" % ( project_namespace, project_name )
+    tag = branch
+    dry_run = False
+    untagged = False
+    prune = True
 
-        try:
-            cleaner = delete_docker_registry_image.RegistryCleaner(registry_data_dir, dry_run)
-            if untagged:
-                cleaner.delete_untagged(image)
+    logger.info("Trying to delete %s:%s" %( image, branch ))
+
+    try:
+        cleaner = delete_docker_registry_image.RegistryCleaner(registry_data_dir, dry_run)
+        if untagged:
+            cleaner.delete_untagged(image)
+        else:
+            if tag:
+                cleaner.delete_repository_tag(image, tag)
             else:
-                if tag:
-                    cleaner.delete_repository_tag(image, tag)
-                else:
-                    cleaner.delete_entire_repository(image)
+                cleaner.delete_entire_repository(image)
 
-            if prune:
-                cleaner.prune()
-        except delete_docker_registry_image.RegistryCleanerError as error:
-            logger.fatal(error)
+        if prune:
+            cleaner.prune()
+    except delete_docker_registry_image.RegistryCleanerError as error:
+        logger.fatal(error)
 
 def main():
     handler = logging.StreamHandler()
